258713b7fd66fca429e485ab006cbeb082d07b92
Piyush Choubey <piyushchoubey370@gmail.com>
2026-08-22 10:36:59 +0530

    Time: 0 ms (100%), Space: 44.4 MB (73.22%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -0,0 +1,57 @@
+// import java.util.ArrayList;
+// import java.util.List;
+// class Solution {
+//     public List<String> generateParenthesis(int n) {
+//         List<String> result = new ArrayList<>();
+//         backtrack(result, "", 0, 0, n);
+//         return result;
+//     }
+//     private void backtrack(List<String> result, String current, int openCount, int closeCount, int n) {
+//         if (openCount == n && closeCount == n) {
+//             result.add(current);
+//             return;
+//         }
+//         if (openCount < n) {
+//             backtrack(result, current + "(", openCount + 1, closeCount, n);
+//         }
+//         if (closeCount < openCount) {
+//             backtrack(result, current + ")", openCount, closeCount + 1, n);
+//         }
+//     }
+// }
+
+// ********another code*******
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        List<String> result = new ArrayList<>();
+        StringBuilder temp = new StringBuilder();
+
+        backtrack(result, temp, 0, 0, n);
+
+        return result;
+    }
+
+    private void backtrack(List<String> result, StringBuilder temp,
+                           int open, int close, int n) {
+
+        if (open == n && close == n) {
+            result.add(temp.toString());
+            return;
+        }
+
+        if (open < n) {
+            temp.append('(');
+            backtrack(result, temp, open + 1, close, n);
+            temp.deleteCharAt(temp.length() - 1);
+        }
+
+        if (close < open) {
+            temp.append(')');
+            backtrack(result, temp, open, close + 1, n);
+            temp.deleteCharAt(temp.length() - 1);
+        }
+    }
+}
